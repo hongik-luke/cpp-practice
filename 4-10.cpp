4c1c20bb9bc42380de353c801fb9a8b6e15434bf
hongik-luke <142500104+hongik-luke@users.noreply.github.com>
2024-04-10 22:47:56 +0900

    백준 할당량 / 많이 헤맨듯? 속상하네

--- 4-10.cpp
+++ 4-10.cpp
@@ -3,180 +3,87 @@
 #include <string>
 #include <cstring>
 #include <cstdlib>
-#include <ctime>
+#include <stack>
+#include <cctype>
+#include <map>
 #define fastio ios::sync_with_stdio(0),cout.tie(0),cin.tie(0)
 using namespace std;
 
-class GameObject {
-protected:
-	int distance;
-	int x, y;
-public:
-	GameObject(int startX, int startY, int distance) {
-		this->x = startX; this->y = startY;
-		this->distance = distance;
-	}
-	virtual ~GameObject() {};
-	virtual void move() = 0;
-	virtual char getShape() = 0;
-
-	int getX() { return x; }
-	int getY() { return y; }
-	bool collide(GameObject *p) {
-		if (this->x == p->getX() && this->y == p->getY())
-			return true;
-		else
-			return false;
-	}
-};
-class Human : virtual public GameObject{
-protected:
-    char name;
-public:
-	Human(int startX, int startY, int distance) :GameObject(startX, startY, distance) {
-	    this->name = 'H';
-	}
-    virtual ~Human() {};
-    virtual void move(){
-        char key;
-		cout << "왼쪽(a), 아래(s), 위(d), 오른쪽(f) >> ";
-		cin >> key;
-        if(key == 'a'){
-            if(y - distance >= 0) y = y - distance;
-            else y = 20 + y - distance;
-        }
-        else if(key == 's'){
-            if(x + distance <=  9) x = x + distance;
-            else x = x + distance - 10;
-        }
-        else if(key == 'd'){
-            if(x - distance >= 0) x = x - distance;
-            else x = 10 + x - distance;
+int main(void){
+    stack <string> pstack;
+    string s;
+    cin>>s;
+    string* str = new string [s.length()];
+    for(int i = 0 ; i < s.length();  i++)
+        str[i] = s.substr(i,1);
+    
+    
+    for(int i = 0 ; i < s.length();  i++){
+        if(str[i] == "(" || str[i] == "["){
+            pstack.push(str[i]);
+            continue;
         }
         else{
-            if(y + distance <= 19) y = y + distance;
-            else y = y + distance - 20;
-            
-        }
-    };
-	virtual char getShape(){
-	    return name;
-	};
-};
-
-class Monster : virtual public GameObject{
-protected:
-    char name;
-public:
-	Monster(int startX, int startY, int distance) :GameObject(startX, startY, distance) {
-	    this->name = 'M';
-	    srand((unsigned)time(0));
-	}
-    virtual ~Monster() {};
-    virtual void move(){
-        int num;
-		num = rand() % 4;
-        if(num == 0){
-            if(y - distance >= 0) y = y - distance;
-            else y = 20 + y - distance;
-        }
-        else if(num == 1){
-            if(x + distance <=  9) x = x + distance;
-            else x = x + distance - 10;
-        }
-        else if(num == 2){
-            if(x - distance >= 0) x = x - distance;
-            else x = 10 + x - distance;
-        }
-        else{
-            if(y + distance <= 19) y = y + distance;
-            else y = y + distance - 20;
-            
-        }
-    }
-	virtual char getShape(){
-	    return name;
-	}
-};
-class Food : virtual public GameObject{
-	int count;
-	char name;
-public:
-	Food(int startX, int startY, int distance) :GameObject(startX, startY, distance) {
-		srand((unsigned)time(0));
-		count = 0;
-		this->name = '@';
-	}
-	void move() {
-		int n1, num;		//n1 : 2/5 확률, n2 : 방향 선택
-		n1 = rand() % 5;
-		num = rand() % 4;
-		if (n1 >= 1 && n1 <= 2){
-		    if(num == 0){
-                if(y - distance >= 0) y = y - distance;
-                else y = 20 + y - distance;
+            if(pstack.empty())
+            {
+                cout<<0;
+                return 0;
             }
-            else if(num == 1){
-                if(x + distance <=  9) x = x + distance;
-                else x = x + distance - 10;
+            int num = 1;
+            if(pstack.top() != "[" && pstack.top() != "("){
+                num = 0;
+                while(!pstack.empty()){
+                    if(pstack.top() != "[" && pstack.top() != "("){
+                        int temp = stoi(pstack.top());
+                        num += temp;
+                        pstack.pop();
+                    }
+                    else 
+                        break;
+                }
             }
-            else if(num == 2){
-                if(x - distance >= 0) x = x - distance;
-                else x = 10 + x - distance;
+            if(pstack.empty())
+            {
+                cout<<0;
+                return 0;
+            }
+            
+            if(str[i] == ")" && pstack.top() == "("){
+                pstack.pop();
+                num = num*2;
+                pstack.push(to_string(num));
+            }
+            else if(str[i] == "]" && pstack.top() == "["){
+                pstack.pop();
+                num = num*3;
+                pstack.push(to_string(num));
             }
             else{
-                if(y + distance <= 19) y = y + distance;
-                else y = y + distance - 20;
+                cout<<0;
+                return 0;
             }
-		}
-	}
-	virtual char getShape(){
-	    return name;
-	}
-};
+        }
 
-class Game{
-public:
-    void game() {
-        Human* h = new Human(0, 0, 1);	Monster *m = new Monster(5, 5, 2);
-		Food* f = new Food(8, 9, 1);
-		cout<<"game start"<<"\n";
-		
-	    while(1){				
-			for (int i = 0; i<10; i++) {
-				for (int j = 0; j<20; j++) {
-				    cout<<"|";
-					if (m->getX() == i && m->getY() == j) cout << m->getShape();	
-					else if (h->getX() == i && h->getY() == j) cout << h->getShape();
-					else if (f->getX() == i && f->getY() == j) cout << f->getShape();
-					else cout << '-';
-					cout<<"|";
-				}
-			    cout << "\n";
-			}
-			
-				
-			if(m->collide(h)){
-				cout<<"Monster catched human, game lose!"<<"\n";
-				break;
-			}
-			if(m->collide(f)){
-				cout<<"Monster catched Food, game lose!"<<"\n";
-				break;
-			}
-            if(h->collide(f)){
-				    cout<<"Human catched Food, game win!"<<"\n";
-				break;
-			}
-			h->move();
-			f->move();
-			m->move();
-	    }
     }
-};
-
-int main(void){
-    Game* g = new Game;
-	g->game();
-	delete g;
+    int result;
+    while(!pstack.empty()){
+        if(pstack.top() == "[" ||pstack.top() == "(" || pstack.top() == "]" ||pstack.top() == ")")
+        {
+            cout<<0;
+            return 0;
+        }
+        int temp2 = stoi(pstack.top());
+        result += temp2;
+        pstack.pop();
+    }
+    cout<<result;
 }
+
+
+
+
+
+
+
+
+
