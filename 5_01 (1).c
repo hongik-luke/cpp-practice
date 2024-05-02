115c6f42def2f507b48ecd101c152390d3b79164
hongik-luke <142500104+hongik-luke@users.noreply.github.com>
2024-05-02 20:56:25 +0900

    아이디어 전체가 내꺼가 아니라서 내가했다기에 애매한

--- 5_01 (1).c
+++ 5_01 (1).c
@@ -1,38 +1,63 @@
 #include <iostream>
 #include <algorithm>
 #include <cstring>
+#include <limits>
+
 using namespace std;
+int M;
+int numarr[501][501] = {0,}; //0부터 유의미 M-1까지 유의미
+int original_num[501];
+int dp[501][501];
 
-int N,M;
-int triangle[110][110];
-int cashe[110][110];
-void get(){
+void get_n_clear(){
     cin>>M;
-    for(int i = 1 ; i <= M ; i++)
-        for(int j = 1 ; j <= i ; j++)
-            cin>>triangle[i][j];
-    memset(cashe,-1,sizeof(cashe));
-}
-int getresult(int down, int next){
+    for(int i = 0 ; i < M ; i++)
+        cin>>original_num[i];
+    for(int i = 0 ; i < M ; i++)
+        numarr[i][i] = original_num[i];
+    for(int i = 0 ; i < M ; i++){
+        for(int j = i+1 ; j < M ; j++){
+            numarr[i][j] = numarr[i][j-1] + original_num[j];
+        }
+    }
+    memset(dp,98765432,sizeof(dp));
     
-    if(down == M)
-        return triangle[down][next];
-        
-    int& ret = cashe[down][next];
-    if(ret != -1)
-        return ret;
-        
-    ret = triangle[down][next] + max(getresult(down+1,next),getresult(down+1,next+1));
-    return ret;
 }
 
-int main(void){
+
+int getresult(){
+    for(int i = 0 ; i < M ; i++)
+        dp[i][i] = 0;
+        
+    for(int i = 1 ; i < M ; i++){
+        for(int j = 0 ; j < M ; j++){
+            //dp[j][j+i] 구하는 중
+            if(j+i >= M)
+                break;
+            for(int p = 0 ; p < i ; p++){
+                
+                dp[j][j+i] = min(dp[j][j+i],dp[j][j+p] + dp[j+p+1][j+i]);
+            }
+            dp[j][j+i] += numarr[j][j+i];
+        }
+    }
+    return dp[0][M-1];
+}
+
+
+
+
+int main(void){ 
+    int N;
     cin>>N;
     for(int i = 0 ; i < N ; i++){
-        get();
-        cout<<getresult(1,1)<<"\n";
+        get_n_clear();
+        cout<<getresult()<<"\n";
     }
+    
 }
+
+
 
 
 
