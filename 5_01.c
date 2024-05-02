c4ef7f5206a8f27d83b72d587b7640d8d310031f
hongik-luke <142500104+hongik-luke@users.noreply.github.com>
2024-05-02 20:54:57 +0900

    시간초과 나네 시발거

--- 5_01.c
+++ 5_01.c
@@ -1,39 +1,61 @@
 #include <iostream>
 #include <algorithm>
 #include <cstring>
+#include <limits>
+
 using namespace std;
-int N,M;
-int numarr[510];
-int cashe[510];
+
+
+int cashe[110][12];
+int len , k;
+int numarr[110];
+
 void get(){
-    cin>>M;
-    for(int i = 0 ; i < M ; i++)
-            cin>>numarr[i];
+    cin>>len>>k;
+    for(int i = 0 ; i < len ; i++)
+        cin>>numarr[i];
+    sort(numarr,numarr+len);
     memset(cashe,-1,sizeof(cashe));
 }
 
+int square(int n){return n*n;}
 
-int check(int index){
+int getresult(int first, int last){
+
+    int minresult = 87654321;
     
-    int& ret = cashe[index+1];
-    if(ret != -1)
-        return ret;
-    ret = 1;
-    for(int i = index+1 ; i < M ; i++){
-        if(index == -1 || numarr[i] > numarr[index] ){
-            ret = max(ret, 1 + check(i));
+    for(int j = numarr[first] ; j <= numarr[last] ; j++){
+        int nowresult = 0;
+        for(int i = 0 ; i <= last-first ; i++){
+            nowresult += square(j - numarr[i+first]);
         }
+        minresult = min(nowresult , minresult);
     }
-    return ret;
+    //cout<<index<<" "<<index+howmany-1<<" "<<minresult<<"\n";
+    return minresult;
 }
-
-
+int seperate(int index , int parts){
+    if(index == len) return 0;
+    if(parts == 0) return 98765443;
+    
+    int ret = cashe[index][parts];
+    if(ret != -1) return ret;   
+    ret = 7654321;
+    
+    for(int i = 1 ; i+index <= len ; i++){
+        ret = min(ret, getresult(index,index+i-1) + seperate(index+i, parts-1));
+        //cout<<index<<" "<<index+i-1<<" "<<index+i<<" "<<parts-1<<" "<<ret<<"\n";
+    }
+    
+    return ret;
+    
+}
 int main(void){
+    int N;
     cin>>N;
-    for(int i = 0 ; i < N ; i++){
+    for(int i = 0 ; i < N ;i++){
         get();
-        cout<<check(-1)-1<<"\n";
-
+        cout<<seperate(0, k)<<"\n";
     }
 }
 
