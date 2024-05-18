f28f42f513eb9283b4a34a9022fbcf0ddbf1e5e2
hongik-luke <142500104+hongik-luke@users.noreply.github.com>
2024-05-18 16:33:30 +0900

    dfs + dp !

--- 5_17.c
+++ 5_17.c
@@ -1,78 +1,58 @@
 #include <iostream>
 #include <algorithm>
 #include <cstring>
-#include <cmath>
 #include <vector>
 #include <queue>
 using namespace std;
+
 const int IMF = 98765432;
-int E;
-int capacitiy[60][60]; //a 0 , z 25, A 26 , Z 61
-int flow[60][60]; // 
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+
+int maxx,maxy;
+int board[55][55];
+bool visited[55][55];
+
 void input(){
-    cin>>E;
-    char c1,c2;
-    int n1,n2,n3;
-    for(int i = 0 ; i < E ; i++){
-        cin>>c1>>c2>>n3;
-        if((int)c1 >= 65 && (int)c1 <= 90)
-            n1 = (int)c1 - 39;
-        else
-            n1 = (int)c1 - 97;
-        
-        if((int)c2 >= 65 && (int)c2 <= 90)
-            n2 = (int)c2 - 39;
-        else
-            n2 = (int)c2 - 97;
-
-        capacitiy[n1][n2] += n3;
-        capacitiy[n2][n1] += n3;
+    cin>>maxx>>maxy;
+    string n;
+    for(int i = 0 ; i < maxx ; i++){
+        cin>>n;
+        for(int j = 0 ; j < maxy ; j++){
+            if(n[j] == 'H')
+                board[i][j] = 0;
+            else
+                board[i][j] = (int)n[j] - 48;
+        }
     }
-
 }
-
-int networkFlow(int source , int sink){
-    memset(flow,0,sizeof(flow));   
-    int totalflow = 0;
+int cashe[55][55];
+int dfs(int cx, int cy){
+    if(visited[cx][cy]) return IMF;
     
-    while(true){
-        vector<int> parent(60,-1);
-        queue <int> q;
-        parent[source] = source;
-        q.push(source);
-        while(!q.empty() && parent[sink] == -1){
-            int here = q.front();
-            q.pop();
-            for(int there = 0 ; there < 52 ; there++){
-                if(capacitiy[here][there] - flow[here][there] > 0 && parent[there] == -1){
-                    q.push(there);
-                    parent[there] = here;
-                }
-            }
-        }
-        
-        
-        if(parent[sink] == -1) break;
-        
-        int amount = IMF;
-        for(int p = sink ; p != source ; p = parent[p])
-            amount = min(capacitiy[parent[p]][p] - flow[parent[p]][p],amount);
-            
-        for(int p = sink ; p != source ; p = parent[p]){
-            flow[parent[p]][p] += amount;
-            flow[p][parent[p]] -= amount;
-        }
-        
-        totalflow += amount;
+    int& returnmax = cashe[cx][cy];
+    if(returnmax != 0) return returnmax;
+    returnmax = 1;
+    visited[cx][cy] = true;
+    for(int i = 0 ; i < 4 ; i++){
+        int nx = cx + dx[i]*board[cx][cy];
+        int ny = cy + dy[i]*board[cx][cy];
+        if(nx < 0 || nx > maxx-1 || ny < 0 || ny > maxy-1 || board[nx][ny] == 0) continue;
+        int result1 = 1+dfs(nx,ny);
+
+        returnmax = max(returnmax,result1);
     }
-        
-    return totalflow;    
+    visited[cx][cy] = false;
+    
+    return returnmax;
 }
 
 int main(void){
+    memset(visited,false,sizeof(visited));
+    memset(cashe,0,sizeof(cashe));
     input();
-    cout<<networkFlow(26,51);
+    int result = dfs(0,0);
+    if(result >= IMF) cout<<-1;
+    else cout<<result;  
 }
-
-
 
