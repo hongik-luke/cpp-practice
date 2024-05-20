5999a29373ceb2677ade56ade3c193281266d017
hongik-luke <142500104+hongik-luke@users.noreply.github.com>
2024-05-20 22:34:27 +0900

    하나만 추가하는게 참 어렵다 / 다른방법 찾아보기

--- 5_19.c
+++ 5_19.c
@@ -1,3 +1,4 @@
+//5719
 #include <iostream>
 #include <algorithm>
 #include <cstring>
@@ -7,105 +8,120 @@
 #define fasti ios_base::sync_with_stdio(false); cin.tie(0);
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 using namespace std;
+struct pos{
+    int value;
+    int idx;
+    int beforeidx;
+};
+struct cmp{
+    bool operator()(pos a ,pos b){
+        return a.value > b.value;
+    }
+};
 
-int arr[7];
-void input(){
-    cin>>arr[1]>>arr[2]>>arr[3]>>arr[4]>>arr[5]>>arr[6];
+int N,M,S,D;
+vector<pos> adj[505];
+int shortcut = -1;
+
+
+bool input(){
+    for(int i = 0 ; i < 505 ; i++){
+        adj[i].clear();
+    }
+    cin>>N>>M;
+    if(N == 0 && M == 0) return true;
+    cin>>S>>D;
+    int n1,n2,n3;
+    pos npos;
+    for(int i = 0 ; i < M ; i++){
+        cin>>n1>>n2>>n3;
+        npos = {n3,n2,n1};
+        adj[n1].push_back(npos);
+    }
+    return false;
+}
+int visited[510];
+vector<pair<int,int>> back_adj[505];
+
+void delete_min(){
+    queue<pair<int,int>> pq;
+    for(int i = 0 ; i < back_adj[D].size() ; i++){
+        pq.push(back_adj[D][i]);
+    }
+    pair<int,int> now;
+
+    while(!pq.empty()){
+        now = pq.front();
+        pq.pop();
+
+        for(int i = 0 ; i < adj[now.first].size() ; i++){
+            if(adj[now.first][i].idx == now.second){
+                //cout<<adj[now.first][i].beforeidx<<" "<<adj[now.first][i].idx<<"\n";
+                adj[now.first].erase(adj[now.first].begin()+i);
+            }
+        }
+
+        for(int i = 0 ; i < back_adj[now.first].size() ; i++){
+            pq.push(back_adj[now.first][i]);
+        }
+        for(int i = 0 ; i < back_adj[now.first].size() ; i++){
+            back_adj[now.first].erase(back_adj[now.first].begin()+i);
+        }
+    }
 }
 
-int getresult(){
-    //6처리
-    int result = arr[6];
 
-    //5처리
-    result += arr[5];
-    if(arr[1] - arr[5]*11 < 0) arr[1] = 0;
-    else arr[1] = arr[1] - arr[5]*11;
-    
-    //4처리
-    result += arr[4];
-    int remain4 = arr[4]*5;
-    if(remain4 - arr[2] > 0){
-        remain4 = (remain4-arr[2])*4;
-        arr[2] = 0;
-        if(arr[1] - remain4 < 0) arr[1] = 0;
-        else arr[1] = arr[1] - remain4;
+int daikstra(){
+    for(int i = 0 ; i < 505 ; i++){
+        back_adj[i].clear();
     }
-    else arr[2] = arr[2] - remain4;
+    memset(visited,-1,sizeof(visited));
+    shortcut = -1;
 
-    //3처리
-    result += arr[3]/4;
-    arr[3] = arr[3]%4;
-    if(arr[3] == 1){
-        result++;
-        arr[3] = 0;
-        if(arr[2] >= 5){
-            arr[2] -= 5;
-            arr[1] -= 7;
-            if(arr[1] < 0) arr[1] = 0;
-        }
-        else{
-            int remain3 = 27;
-            remain3 -= arr[2]*4;
-            arr[2] = 0;
-            arr[1] -= remain3;
-            if(arr[1] < 0) arr[1] = 0;
-        }
-    }
-    else if(arr[3] == 2){
-        result++;
-        arr[3] = 0;
-        if(arr[2] >= 3){
-            arr[2] -= 3;
-            arr[1] -= 6;
-            if(arr[1] < 0) arr[1] = 0;
-        }
-        else{
-            int remain3 = 18;
-            remain3 -= arr[2]*4;
-            arr[2] = 0;
-            arr[1] -= remain3;
-            if(arr[1] < 0) arr[1] = 0;
-        }
-    }
-    else if(arr[3] == 3){
-        result++;
-        arr[3] = 0;
-        if(arr[2] >= 1){
-            arr[2] -= 1;
-            arr[1] -= 5;
-            if(arr[1] < 0) arr[1] = 0;
-        }
-        else{
-            int remain3 = 9;
-            arr[2] = 0;
-            arr[1] -= remain3;
-            if(arr[1] < 0) arr[1] = 0;
-        }
-    }
-    
-    //2처리
-    result += (arr[2]*4)/36;
-    arr[1] += arr[2]*4%36;
-    
-    //1처리
-    result += arr[1]/36;
-    if(arr[1]%36 != 0) result++;
-    return result;
-}
+    priority_queue<pos,vector<pos>,cmp> pq;
+    pos cpos,npos;
+    cpos = {0,S,S};
+    pq.push(cpos);
 
+    while(!pq.empty()){
+        cpos = pq.top();
+        pq.pop();
+
+        if(cpos.idx == D){
+            if(shortcut == -1 || shortcut == cpos.value){
+                back_adj[cpos.idx].push_back(make_pair(cpos.beforeidx, cpos.idx));
+                shortcut = cpos.value;
+                continue;
+            }
+            else break;
+        }
+        if(visited[cpos.idx] == -1 || visited[cpos.idx] == cpos.value){
+            back_adj[cpos.idx].push_back(make_pair(cpos.beforeidx, cpos.idx));
+        }
+
+        if(visited[cpos.idx] != -1) continue;
+        visited[cpos.idx] = cpos.value;
+
+        //cout<<cpos.value<<" "<<cpos.idx<<" "<<cpos.beforeidx<<"\n";
+        for(int i = 0 ; i < adj[cpos.idx].size() ; i++){
+            if(visited[adj[cpos.idx][i].idx] != -1) continue;
+            npos = {cpos.value + adj[cpos.idx][i].value, adj[cpos.idx][i].idx, cpos.idx};
+            pq.push(npos);
+        }
+    }
+
+    delete_min();
+    if(shortcut != -1) return shortcut;
+    else return -1;
+}   
 
 int main(void){
-    input();
-    cout<<getresult();
+    int print_result;
+    while(true){
+        if(input()) break;
+        daikstra();
+        cout<<daikstra()<<"\n";
+    }
 }
-
-
-
-
-
-
-
-
 
 
