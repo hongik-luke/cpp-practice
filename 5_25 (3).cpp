0069fc78883543aa6ccfe37a8a6d2f8fc35ac927
hongik-luke <142500104+hongik-luke@users.noreply.github.com>
2024-05-25 13:39:07 +0900

    이분탐색. 범위를 잘보자!

---
+++ 5_25 (3).cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+int64_t N,K;
+int arr[10010];
+
+void input(){
+    cin>>N>>K;
+    for(int i = 0 ; i < N ; i++){
+        cin>>arr[i];
+    }
+}
+int64_t howmany(int n){
+    int64_t returnnum = 0;
+    for(int i = 0 ; i < N ; i++){
+        returnnum += arr[i]/n;
+    }
+    return returnnum;
+}
+
+
+int find(){
+    int64_t lo = 1; int64_t hi = 100000000000;
+    int64_t mid;
+    while(lo < hi){
+        mid = (lo+hi)/2;
+        if(howmany(mid) < K)
+            hi = mid;
+        else
+            lo = mid+1;
+    }
+    return lo-1;
+}
+
+
+int main(void){
+    input();
+    cout<<find();
+}
+
+
