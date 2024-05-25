e047db82ef8eb2242a47e88d94be41e695b959c9
hongik-luke <142500104+hongik-luke@users.noreply.github.com>
2024-05-25 16:02:13 +0900

    [Gold V] Title: 용액, Time: 64 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <cmath>
+using namespace std;
+int N;
+vector<int> minusv;
+vector<int> plusv;
+
+void input(){
+    cin>>N;
+    int n;
+    for(int i = 0 ; i < N ; i++){
+        cin>>n;
+        if(n < 0) minusv.push_back(n);
+        else plusv.push_back(n);
+    }
+}
+
+void result(){
+    int pidx1,pidx2;
+    int returnmin = 2000000001;
+    
+    if(minusv.size() >= 2){
+        returnmin = abs(minusv[minusv.size()-2]+minusv[minusv.size()-1]);
+        pidx1 = minusv[minusv.size()-2];
+        pidx2 = minusv[minusv.size()-1];
+    }
+    if(plusv.size() >= 2){
+        if(returnmin > abs(plusv[0]+plusv[1])){
+            returnmin = abs(plusv[0]+plusv[1]);    
+            pidx1 = plusv[0];
+            pidx2 = plusv[1];
+        }
+    }
+    
+    for(int i = 0 ; i < minusv.size() ; i++){
+        if(plusv.size() == 0) break;
+        int lo = 0 ; int hi = plusv.size()-1;
+        int mid;
+        while(lo < hi){
+            mid = (lo + hi)/2;
+            if(minusv[i] + plusv[mid] > 0)
+                hi = mid;
+            else
+                lo = mid+1;
+        }
+
+        if(returnmin > abs(minusv[i]+plusv[lo])){
+            returnmin = abs(minusv[i]+plusv[lo]);
+            pidx1 = minusv[i];
+            pidx2 = plusv[lo];
+        }
+    
+        if(lo != 0){
+            if(returnmin > abs(minusv[i]+plusv[lo-1])){
+                returnmin = abs(minusv[i]+plusv[lo-1]);
+                pidx1 = minusv[i];
+                pidx2 = plusv[lo-1];
+            }
+        }
+        //cout<<pidx1<<pidx2<<"\n";
+    }
+    
+    cout<<pidx1<<" "<<pidx2;
+}
+
+int main(void){
+    input();
+    result();
+}
