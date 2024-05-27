f3a7788ace32dd3cefc99451a2d0eb244c893988
luke <142500104+hongik-luke@users.noreply.github.com>
2024-05-27 20:12:38 +0900

    쨔스! 재귀호출이 너무 많은게 문제였어!!

---
+++ 5_27 (1).cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+using namespace std;
+int value[1010];
+int N,K,to_get;
+vector<int> v[1010];
+int64_t cashe[1010];
+void input(){
+    cin>>N>>K;
+    for(int i = 1 ; i <= N ; i++){
+        cin>>value[i];
+    }
+    int n1,n2;
+    for(int i = 1 ; i <= K ; i++){
+        cin>>n1>>n2;
+        v[n2].push_back(n1);
+    }
+    cin>>to_get;
+}
+
+int64_t dp(int idx){
+    int64_t& ret = cashe[idx];
+    if(ret != -1) ret;
+    ret = 0;
+    
+    for(int i = 0 ; i < v[idx].size() ; i++){
+        if(cashe[v[idx][i]] != -1) ret = max(ret,cashe[v[idx][i]]);
+        else ret = max(ret,dp(v[idx][i]));
+    }
+
+    ret += value[idx];
+    return ret;
+}
+
+int main(void){
+    int TC;
+    cin>>TC;
+    for(int i = 0 ; i < TC ; i++){
+        memset(cashe,-1,sizeof(cashe));
+        for(int i = 0 ; i <1010 ; i++){
+            v[i].clear();
+        }
+        input();
+        cout<<dp(to_get)<<"\n";
+    }
+}
+
+
