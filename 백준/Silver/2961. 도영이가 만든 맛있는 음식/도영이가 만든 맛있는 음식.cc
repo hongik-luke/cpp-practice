05f447d7976d2f68b1db333b4df1007ea1d57186
luke <142500104+hongik-luke@users.noreply.github.com>
2024-05-29 15:02:49 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <cmath>
+using namespace std;
+int N;
+pair<int,int> arr[15];
+void input(){
+    cin>>N;
+    for(int i = 0; i < N ; i++){
+        cin>>arr[i].first>>arr[i].second;
+    }
+}
+
+int get(){
+    int result = 2000000000;
+    
+    for(int i = (1<<N)-1 ; i>0 ; i--){
+        int S = 1, B = 0;
+        for(int j = 0 ; j < N ; j++){
+            if(i & (1<<j)){
+                S *= arr[j].first;
+                B += arr[j].second;
+            }    
+        }
+        result = min(result,abs(S-B));
+    }
+    return result;
+}
+int main(void){
+    input();
+    cout<<get();
+}
