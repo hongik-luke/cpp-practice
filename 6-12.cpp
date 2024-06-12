4d9c586ff425e5fb46cc239b152e705192dc2c7c
luke <142500104+hongik-luke@users.noreply.github.com>
2024-06-12 22:43:49 +0900

    이상한 깨달음을 얻었다?

--- 6-12.cpp
+++ 6-12.cpp
@@ -3,83 +3,85 @@
 #include <cstring>
 #include <queue>
 using namespace std;
-int board[104][104];
-bool check[104];
-int N,L;
+int dx[] = {-1,-1,-1,0,0,1,1,1};
+int dy[] = {-1,0,1,-1,1,-1,0,1};
+vector<int> tree[13][13];
+int board[13][13];
+int plus_[13][13];
+int N,M,K;
 void input(){
-    cin>>N>>L;
-    for(int i = 0 ; i < N ; i++){
-        for(int j = 0 ; j < N ; j++){
-            cin>>board[i][j];
+    cin>>N>>M>>K;
+
+    for(int i = 1 ; i <= N ; i++)
+        for(int j = 1 ; j <= N ; j++){
+            cin>>plus_[i][j];
+            board[i][j] = 5;
         }
+    for(int i = 0 ; i < M ; i++){
+        int n1,n2,n3;
+        cin>>n1>>n2>>n3;
+        tree[n1][n2].push_back(n3);
     }
-}
-bool get_row(int idx){
-    memset(check,false,sizeof(check));
-    for(int i = 0 ; i < N-1 ; i++){
-        if(board[idx][i] == board[idx][i+1]) continue;
-        
-        if(board[idx][i] > board[idx][i+1]){
-            if(board[idx][i] != board[idx][i+1]+1) return false;
-            int cnt = 0;
-            while(cnt != L){
-                cnt++;
-                if(i+cnt < 0 || i+cnt > N-1) return false;
-                if(check[i+cnt] == true) return false;
-                check[i+cnt] = true;
-            }
-        }
-        
-        if(board[idx][i] < board[idx][i+1]){
-            if(board[idx][i]+1 != board[idx][i+1]) return false;
-            
-            int cnt = 0;
-            while(cnt != L){
-                cnt++;
-                if(i+1-cnt < 0 || i+1-cnt > N-1) return false;
-                if(check[i+1-cnt] == true) return false;
-                check[i+1-cnt] = true;
-            }
-        }
-    }
-    return true;
-}
-bool get_column(int idx){
-    memset(check,false,sizeof(check));
-    for(int i = 0 ; i < N-1 ; i++){
-        if(board[i][idx] == board[i+1][idx]) continue;
-        
-        if(board[i][idx] > board[i+1][idx]){
-            if(board[i][idx] != board[i+1][idx]+1) return false;
-            int cnt = 0;
-            while(cnt != L){
-                cnt++;
-                if(i+cnt < 0 || i+cnt > N-1) return false;
-                if(check[i+cnt] == true) return false;
-                check[i+cnt] = true;
-            }
-        }
-        
-        if(board[i][idx] < board[i+1][idx]){
-            if(board[i][idx]+1 != board[i+1][idx]) return false;
-            
-            int cnt = 0;
-            while(cnt != L){
-                cnt++;
-                if(i+1-cnt < 0 || i+1-cnt > N-1) return false;
-                if(check[i+1-cnt] == true) return false;
-                check[i+1-cnt] = true;
-            }
-        }
-    }
-    return true;
+
 }
 int main(void){
     input();
-    int result_cnt = 0;
-    for(int i = 0 ; i < N ; i++){
-        if(get_row(i)) result_cnt++;
-        if(get_column(i)) result_cnt++;
+    for(int k = 0 ;  k < K ; k++){
+        // 봄
+        for(int i = 1 ; i <= N ; i++){
+            for(int j = 1 ; j <= N ; j++){
+                int for_summer = 0;
+                int cnt = 0;
+                int len = tree[i][j].size();
+                if(len == 0 ) continue;
+                for(int t = 0 ; t < len ; t++){
+                    if(tree[i][j][t] <= board[i][j]){
+                        board[i][j] -= tree[i][j][t];
+                        tree[i][j][t]++;
+                    }
+                    else{
+                        for_summer += tree[i][j][t]/2;
+                        cnt++;
+                    }
+                }
+                
+                //여름 +봄 삭제
+                board[i][j] += for_summer;
+                for(int t = 0 ; t < cnt ; t++){
+                    tree[i][j].pop_back();
+                }
+            }
+        }
+        //가을 for문 5중첩 이거맞나 시발?
+        for(int i = 1 ; i <= N ; i++){
+            for(int j = 1 ; j <= N ; j++){
+                int len = tree[i][j].size();
+                for(int t = 0 ; t < len ; t++){
+                    if(tree[i][j][t]%5 == 0){
+                        int nx,ny;
+                        for(int m = 0 ; m < 8 ; m++){
+                            nx = dx[m] + i;
+                            ny = dy[m] + j;
+                            if(nx < 1 || nx > N || ny < 1 || ny > N) continue;
+                            tree[nx][ny].push_back(1);
+                        }
+                    }
+                }
+            }
+        }
+        //겨울 양분주기 및 정렬
+        for(int i = 1 ; i <= N ; i++){
+            for(int j = 1 ; j <= N ; j++){
+                board[i][j] += plus_[i][j];
+                sort(tree[i][j].begin(),tree[i][j].end());
+            }
+        }
+        
+        
     }
-    cout<<result_cnt;
+    int result = 0;
+    for(int i = 1 ; i <= N ; i++)
+        for(int j = 1 ; j <= N ; j++)
+            result += tree[i][j].size();
+    cout<<result;
 }
