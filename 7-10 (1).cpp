3e4ca09e6b8850d9150c324d4d56b96e96d44b7b
luke <142500104+hongik-luke@users.noreply.github.com>
2024-07-10 20:29:55 +0900

    이분탐색은 언제봐도 신기하단 말이지

---
+++ 7-10 (1).cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <stack>
+#include <cstdlib>
+using namespace std;
+
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+int len = 0;
+int N;
+int ori[1000001];
+int gett[1000001];
+void input(){
+    cin>>N;
+    for(int i = 0 ; i < N;  i++){
+        cin>>ori[i];
+    }
+}
+int main(void){
+    FASTIO;
+    input();
+    gett[0] = ori[0];
+    len++;
+    for(int i = 1 ; i < N ; i++){
+        int lo = 0; int hi = len-1;
+        if(ori[i] > gett[hi]){
+            gett[len] = ori[i];
+            len++;
+        }
+        else{
+            int mid;
+            while(lo <= hi){
+                mid = (lo+hi)/2;
+                if(gett[mid] == ori[i]){
+                    lo = mid;
+                    break;
+                }
+                else if(gett[mid] > ori[i]) hi = mid-1;
+                else lo = mid+1;
+            }
+            gett[lo] = ori[i];
+        }
+    }
+    cout<<len;
+}
+
+
+
+
+
