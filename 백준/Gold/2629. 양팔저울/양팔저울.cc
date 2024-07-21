194e94713846bd71c63f894269cae94816d52cfd
luke <142500104+hongik-luke@users.noreply.github.com>
2024-07-21 16:22:51 +0900

    [Gold III] Title: 양팔저울, Time: 0 ms, Memory: 3272 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cstdlib>
+
+
+
+using namespace std;
+
+
+int N,M;
+int weight[31];
+int have_to_find[10];
+
+bool dp[31][40100];
+bool check[40100];
+int nowlimitweight = 0;
+
+
+void get(){
+    cin>>N;
+    for(int i = 0 ; i < N ; i++)
+        cin>>weight[i];
+    
+    memset(dp,0,sizeof(dp));
+    memset(check,false,sizeof(check));
+    
+    cin>>M;
+    for(int i = 0 ; i < M ; i++)
+        cin>>have_to_find[i];
+}
+
+void getresult(int currentmarble, int currentWeight) {
+	if (currentmarble > N)
+		return;
+	if (dp[currentmarble][currentWeight] != 0)
+		return;
+
+	dp[currentmarble][currentWeight] = true;
+
+	getresult(currentmarble + 1, currentWeight + weight[currentmarble]);
+	getresult(currentmarble + 1, currentWeight);
+	getresult(currentmarble + 1, abs(currentWeight - weight[currentmarble]));
+}
+
+
+void check_result(){
+    for(int i = 0 ; i < M ;i++){
+        if(dp[N][have_to_find[i]])
+            cout<<"Y ";
+        else
+            cout<<"N ";
+    }
+}
+
+int main(void){
+    get();
+    getresult(0,0);
+    check_result();
+}
