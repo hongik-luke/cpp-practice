0ccbbacf44e8efff8f4073d3d9ebf1a9c3c70774
luke <142500104+hongik-luke@users.noreply.github.com>
2024-07-30 19:32:34 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 152 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.cc
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.cc
@@ -1,80 +1,71 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 #include <queue>
+#include <cstring>
+#include <algorithm>
+
+#define FastIO cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(false);
 
 using namespace std;
 int N,M;
-int arr[1000001];
-int seg[4000000];
+int64_t mod = 1000000007;
+int64_t arr[1000010];
 void input(){
-    int a1,a2;
-    cin>>N>>a1>>a2;
-    M = a1+a2;
-    for(int i = 0 ; i < N; i++){
+    int n1,n2;
+    cin>>N>>n1>>n2;
+    M = n1+n2;
+    for(int i = 1 ; i <= N ; i++)
         cin>>arr[i];
-    }
 }
-int get_seg(int idx, int lo, int hi){
+int64_t seg[4000010];
+void make_segtree(int lo, int hi, int idx){
     if(lo == hi){
         seg[idx] = arr[lo];
-        return seg[idx];
+        return;
     }
     int mid = (lo+hi)/2;
-    int64_t num = get_seg(idx*2,lo,mid);
-    num = (num*get_seg(idx*2+1,mid+1,hi)) % 1000000007;
-    seg[idx] = num;
-    return seg[idx];
-}
-int n1,n2,n3;
-int change(int idx, int lo, int hi){
-    if(lo == n2 && hi == n2){
-        seg[idx] = n3;
-        return seg[idx];
-    }
-    int mid = (lo+hi)/2;
-    int64_t num;
-    if(lo <= n2 && n2 <= mid){
-        num = change(idx*2,lo,mid);
-        num = (num*seg[idx*2+1]) % 1000000007;
-    }
-    else if(mid+1 <= n2 && n2 <= hi){
-        num = seg[idx*2];
-        num = (num*change(idx*2+1,mid+1,hi)) % 1000000007;
-    }
-    seg[idx] = num;
-    return seg[idx];
-}
-int get(int idx, int lo, int hi){
+    make_segtree(lo, mid, idx*2+1);
+    make_segtree(mid+1, hi, idx*2+2);
     
-    if(n2 <= lo && hi <= n3) return seg[idx];
-    else if(hi < n2 || n3 < lo) return 1;
-    else{
-        int mid = (lo+hi)/2;
-        int64_t num;
-        num = get(idx*2,lo,mid);
-        num = (num*get(idx*2+1,mid+1,hi)) % 1000000007;
-        return num;
-    }
+    seg[idx] = seg[idx*2+1]*seg[idx*2+2]%mod;
+    
+    return;
 }
-
-void problem(){
+void change_segtree(int lo, int hi ,int idx, int findidx, int num){
+    if(lo == hi && lo == findidx){
+        seg[idx] = num;
+        return;
+    }
+    int mid = (lo+hi)/2;
+    if(lo <= findidx && findidx <= mid)
+        change_segtree(lo, mid, idx*2+1, findidx, num);
+    else
+        change_segtree(mid+1, hi, idx*2+2, findidx, num);
+    
+    seg[idx] = seg[idx*2+1]*seg[idx*2+2]%mod;
+    
+    return;
+}
+int64_t find_segtree(int lo, int hi ,int l, int r, int idx){
+    if(l <= lo && hi <= r) return seg[idx];
+    if(r < lo || hi < l) return 1;
+    int mid = (lo+hi)/2;
+    return find_segtree(lo,mid,l,r,idx*2+1)*find_segtree(mid+1,hi,l,r,idx*2+2)%mod;
+}
+void print(){
+    int n1,n2,n3;
     for(int i = 0 ; i < M ; i++){
         cin>>n1>>n2>>n3;
         if(n1 == 1){
-            n2 = n2-1;
-            change(1,0,N-1);
+            change_segtree(1,N,0,n2,n3);
         }
         else{
-            n2 = n2-1;
-            n3 = n3-1;
-            cout<<get(1,0,N-1)<<"\n";
+            cout<<find_segtree(1,N,n2,n3,0)<<"\n";
         }
     }
 }
-int main(void){
+int main(){
+    FastIO;
     input();
-    get_seg(1,0,N-1);
-    problem();
+    make_segtree(1,N,0);
+    print();
 }
-
